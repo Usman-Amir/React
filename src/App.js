a747853409d3c05db5913020098dc640c586d911
UsmanAmir <muhammad.usman.amir@exact.com>
2018-09-11 03:40:10 +0800

    show searched text in div

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  constructor() {
+    super();
+    this.state = {
+      searchtext: "",
+    }
+  }
+ 
+  onSearch= (sender) => {
+this.setState({
+ searchtext: sender.target.value
+});
+  }
+  
+  
+  render(){
+    return( 
+      <div>
+        <input type="text" onChange={this.onSearch} />
+        <div>
+            {this.state.searchtext}
+        </div>
+        <li>
+          <ul>
+            France
+          </ul>
+          <ul>
+            Egypt
+          </ul>
+          <ul>
+            Pakistan
+          </ul>
+          <ul>
+            India
+          </ul>
+          <ul>
+            Srilanka
+          </ul>
+          <ul>
+            United Arab Emirates
+          </ul>
+          <ul>
+            Malaysia
+          </ul>
+        </li>
       </div>
     );
   }
+
 }
 
 export default App;
